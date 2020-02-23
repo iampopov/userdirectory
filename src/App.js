4b55393682e19c0482ecd866c2c624f85d16b0a7
iampopov <ivantsar@gmail.com>
2020-02-22 17:40:51 -0700

    added bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Container } from "reactstrap"; //, Row, Col
+
+
 import NavBar from "./components/NavBar";
 import EmployeeView from "./components/EmployeeView";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <EmployeeView />
+      <Container>
+        <NavBar />
+        <EmployeeView />
+
+      </Container>
     </div>
   );
 }
