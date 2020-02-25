f50a755d89e13456b186f23fa75d60310386a9f9
iampopov <ivantsar@gmail.com>
2020-02-24 20:43:45 -0700

    deleted comments in app.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import EmployeeCard from "./components/EmployeeCard";
 import sampleData from "./data/sample.json";
 import moment from 'moment';
 
-// I’d create a new value in state called searchResults. Anytime a user searched, I’d take the main list of employees from state and filter it to create the searchResults, then I’d store the updated searchResults in state.
-
-// Then in your JSX you could check to see if searchResults has a length (and thus results), and if it does, show it instead of the main list of employees.
-
-// To make that work, though, you’d need to set searchResults equal to [] when a user was not searching.
-
-// (That way it would default to showing the original employee list instead.
-
 function App() {
   const [employee, setEmployee] = useState({
     searchTerm: "",
@@ -26,7 +18,6 @@ function App() {
   const { searchTerm, data, searchResults } = employee;
 
   const handleInputChange = e => {
-    // const { name, value } = e.target;
     const searchResults = data.filter(employee => employee.email.toLowerCase().includes(e.target.value.toLowerCase()));
     setEmployee({ ...employee, searchResults, searchTerm: e.target.value});
   }
@@ -41,11 +32,9 @@ function App() {
         image = {record.picture.large}
         first = {record.name.first}
         last = {record.name.last}
-        //phone = {(record.phone).replace(/\D/g,'').substring(0,10).match(/^(\d{3})(\d{3})(\d{4})$/)}
         phone={record.phone}
         email = {record.email}
         dob = {moment(record.dob.date, "YYYY-MM-DD[T]HH:mm:ss.SSS[Z]").format("MMMM Do YYYY")} 
-        //dob = {record.dob.date}
         />
       ))
     } else {
@@ -57,7 +46,6 @@ function App() {
       image = {record.picture.large}
       first = {record.name.first}
       last = {record.name.last}
-      //phone = {(record.phone).replace(/\D/g,'').substring(0,10).match(/^(\d{3})(\d{3})(\d{4})$/)}
       phone={record.phone}
       email = {record.email}
       dob = {moment(record.dob.date, "YYYY-MM-DD[T]HH:mm:ss.SSS[Z]").format("MMMM Do YYYY")} 
